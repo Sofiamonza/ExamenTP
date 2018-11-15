d8b58be8a746c3838320085a4d949aaf8c6e1061
Sofiamonza <40038825+Sofiamonza@users.noreply.github.com>
2018-11-15 10:13:09 -0300

    Agrego los elementos necesarios para el trabajo

--- MCG/src/CargarExamen.php
+++ MCG/src/CargarExamen.php
@@ -1,8 +1,10 @@
 <?php
-namespace MCG;
+
+
+require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\Yaml\Yaml;
 
-$yaml = Yaml::parseFile('material/preguntas.yml');
+$yaml = Yaml::parseFile('\material\preguntas.yml');
 $yaml = $yaml['preguntas'];
 

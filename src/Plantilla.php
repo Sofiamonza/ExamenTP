1a70b5c51af90e5667b70188dbbf7cadb3408394
GianinaGilio <gianinagilio@gmail.com>
2018-11-25 11:14:52 -0300

    Cambio todo

--- src/Plantilla.php
+++ src/Plantilla.php
@@ -1,20 +1,47 @@
 <?php
-
 namespace ExamenTP;
-
-use Twig_Environment;
-use Twig_Loader_Filesystem;
 
 class Plantilla {
 
 
     public function plantilla_alumno($cant_preguntas, $array_respuestas, $array_descripcion, $nro_tema){
+		<html>
+			<head>
+				<meta charset="UTF-8"/>
+				<meta name="viewport" content="with=device-width; initial-sacle=1.0" />
+				<title> Examen </title>
+			</head>
+		
+				echo "<h1>Tema: " $nro_tema " </h1>"
+				<ul>
+				<?php foreach ($arraydescripcion as $array_descripcion): ?>
+					<div>
+						<li><?php echo $array_descripcion ?></li>
+						<li><?php echo $array_respuestas ?></li>
+					</div>
+				<?php endforeach; ?>
 
-        echo "<html><h1>Tema: " $nro_tema " </h1>"
-
-    }
+		</html>
+	}
 
     public function plantilla_profesor($preguntas, $cant_preguntas, $nro_tema){
-        
+		<html>
+			<head>
+				<meta charset="UTF-8"/>
+				<meta name="viewport" content="with=device-width; initial-sacle=1.0" />
+				<title> Examen </title>
+			</head>
+		
+				echo "<h1>Tema: " $nro_tema " </h1>"
+				<ul>
+				<?php foreach ($arraydescripcion as $array_descripcion): ?>
+					<div>
+						<li><?php echo $array_descripcion ?></li>
+						<li><?php echo $array_respuestas ?></li>
+					</div>
+				<?php endforeach; ?>
+
+		</html>
+	}
     }
 }
